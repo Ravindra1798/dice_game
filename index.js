76d8d82f83026afa5e3ce563971dd3e16461e5cc
Ravindra1798 <90673948+Ravindra1798@users.noreply.github.com>
2021-10-05 17:30:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 var randomNumber1=Math.floor(Math.random() * 6) + 1; //random number 1 to 6;
 var dicestring = "dice" + randomNumber1 + ".png"; //dice1 to dice 6;
-var randomimgsource1 = "images/" + dicestring;
+var randomimgsource1 =  dicestring;
 var image1 = document.querySelectorAll("img")[0].setAttribute("src",randomimgsource1);
 
 
 var randomNumber2 =Math.floor(Math.random() * 6) + 1;
-var randomimgsource2="images/dice" + randomNumber2 + ".png";
+var randomimgsource2="dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1].setAttribute("src",randomimgsource2);
 
 //if playr 1 wins;
